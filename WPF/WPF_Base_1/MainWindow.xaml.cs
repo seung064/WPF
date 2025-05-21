c4d69090edc08d63b5d121b4f27fbef9dce7f7fd
seung064 <gli056@naver.com>
2025-05-21 10:01:00 +0900

    1

--- WPF/WPF_Base_1/MainWindow.xaml.cs
+++ WPF/WPF_Base_1/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace WPF_Base_1;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : NavigationWindow
 {
     public MainWindow()
     {
